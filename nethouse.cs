3a8b251bcc2e9e92ab20ff74f20f3431b0ccc5e9
al-metal <al-metal@bk.ru>
2016-11-10 15:51:40 +0400

    Получение кол-ва товара

--- nethouse.cs
+++ nethouse.cs
@@ -72,6 +72,8 @@ namespace Bike18
                 otv = webRequest.PostRequest(cookie, "http://bike18.nethouse.ru/api/catalog/getproduct?id=" + productId);
                 string slug = new Regex("(?<=\",\"slug\":\").*?(?=\")").Match(otv).ToString();
                 string balance = new Regex("(?<=,\"balance\":).*?(?=,\")").Match(otv).ToString();
+                if (balance.Contains("\""))
+                    balance = balance.Replace("\"", "");
                 string productCastomGroup = new Regex("(?<=productCustomGroup\":).*?(?=,\")").Match(otv).ToString();
                 string discountCoast = new Regex("(?<=discountCost\":\").*?(?=\")").Match(otv).Value;
                 string serial = new Regex("(?<=serial\":\").*?(?=\")").Match(otv).Value;
